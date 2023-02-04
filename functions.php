61da195711e2883b7f4d565e3960b9f637e4f298
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-04 12:40:05 +0800

    work

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@ include_once 'inc/remove-no-need.php';
 
 include_once 'inc/nav.php';
 
+include_once 'inc/acf.php';
+
 include_once 'inc/cpt-concern.php';
 include_once 'inc/cpt-insight.php';
 include_once 'inc/cpt-press.php';
