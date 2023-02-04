4fb1f01e81e4bc8310598a2ac88c3def3a79b0a2
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-04 21:45:10 +0800

    work

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -15,6 +15,7 @@ get_header();
       $press_features = get_field('press_features', 'option');
       $our_ethos = get_field('our_ethos');
       $meet_our_doctors = get_field('meet_our_doctors');
+      $four_column_content = get_field('4_column_content', 'option');
   ?>
         <div class="module-topFold">
           <div class="textWrap">
@@ -155,6 +156,39 @@ get_header();
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="module-4ColListing mob-hide">
+          <div class="g">
+            <div class="r">
+              <div class="lg-12">
+                <div class="sectionTitleStyling">
+                  <h5><?php echo $four_column_content['headline']; ?></h5>
+                  <!-- <h2>Evidence-based options that produce real results</h2> -->
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="g">
+            <div class="r rowMargin">
+              <?php
+                foreach($four_column_content['column'] as $column){
+              ?>
+                  <div class="mdlg-3 md-6">
+                    <div class="eachThumb">
+                      <div class="mediaWrapStyling">
+                        <img src="<?php echo aq_resize($column['image']['url'], 50); ?>" data-hiResImg="<?php echo $column['image']['url']; ?>" />
+                      </div>
+                      <h4><?php echo $column['headline']; ?></h4>
+                      <p><?php echo $column['description']; ?></p>
+                    </div>
+                  </div>
+              <?php
+                }
+              ?>
             </div>
           </div>
         </div>
