800bc1bdce837e208683a0ba3d2ae75abdb13bef
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-04 22:57:37 +0800

    work

--- page-templates/page-home.php
+++ page-templates/page-home.php
@@ -192,6 +192,110 @@ get_header();
             </div>
           </div>
         </div>
+
+        <div class="module-4ColListing">
+          <div class="g">
+            <div class="r">
+              <div class="lg-12">
+                <div class="sectionTitleStyling">
+                  <h5>Our treatments</h5>
+                  <h2>Evidence-based options that produce real results</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="g">
+            <div class="r rowMargin">
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+              <div class="mdlg-3 md-6">
+                <a href="single-treatment.html" class="eachThumb hoverEffect_dim">
+                  <div class="mediaWrapStyling">
+                    <img src="assets/demo/small/demo-thumb-short-01.jpg" data-hiResImg="assets/demo/demo-thumb-short-01.jpg" />
+                  </div>
+                  <h4>Rejuran</h4>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="g">
+            <div class="r">
+              <div class="lg-12">
+                <div class="btnsWrap">
+                  <a href="treatments.html" class="buttonWithArrowStyling buttonWithArrowStyling_red">
+                    <p>View more</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 14.795">
+                      <path id="Path_97" data-name="Path 97" d="M1.1-8.621v1.8h17.12v.872A4.906,4.906,0,0,0,15.4-4.446l-2.88,2.88L13.759-.325l7.345-7.4-7.345-7.4-1.242,1.242L15.5-10.919a4.814,4.814,0,0,0,2.721,1.427v.872Z" transform="translate(-1.104 15.12)" fill="#dad9d7" />
+                    </svg>
+                  </a>
+
+                  <a href="#" class="buttonWithArrowStyling buttonWithArrowStyling_outline">
+                    <p>Consult our doctors</p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 14.795">
+                      <path id="Path_97" data-name="Path 97" d="M1.1-8.621v1.8h17.12v.872A4.906,4.906,0,0,0,15.4-4.446l-2.88,2.88L13.759-.325l7.345-7.4-7.345-7.4-1.242,1.242L15.5-10.919a4.814,4.814,0,0,0,2.721,1.427v.872Z" transform="translate(-1.104 15.12)" fill="#dad9d7" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
   <?php
     }
   ?>
