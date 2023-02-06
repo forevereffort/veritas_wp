5ec270a0daf49281d2edf7c88cf220a248987faa
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-06 12:31:36 +0800

    work

--- page-templates/page-concerns.php
+++ page-templates/page-concerns.php
@@ -13,27 +13,7 @@ get_header();
 
       $top_fold_banner = get_field('top_fold_banner');
   ?>
-      <div class="module-topFold">
-        <div class="textWrap">
-          <div class="g">
-            <div class="r">
-              <div class="mdlg-6 md-8 sm-12">
-                <div class="topfoldWrap">
-                  <h1><?php the_title(); ?></h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="mediaWrapStyling asCover">
-          <!-- desktop img -->
-          <img class="desk" src="<?php echo aq_resize($top_fold_banner['desktop_background_image']['url'], 50); ?>" data-hiResImg="<?php echo $top_fold_banner['desktop_background_image']['url']; ?>" />
-          <!-- table img -->
-          <img class="tab" src="<?php echo aq_resize($top_fold_banner['tablet_background_image']['url'], 50); ?>" data-hiResImg="<?php echo $top_fold_banner['tablet_background_image']['url']; ?>" />
-          <!-- mobile img -->
-          <img class="mob" src="<?php echo aq_resize($top_fold_banner['mobile_background_image']['url'], 50); ?>" data-hiResImg="<?php echo $top_fold_banner['mobile_background_image']['url']; ?>" />
-        </div>
-      </div>
+      <?php get_template_part('page-section/module', 'topFold-page', array('top_fold_banner' => $top_fold_banner)); ?>
 
       <div class="grid-list">
         <div class="g">
