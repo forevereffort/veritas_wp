2d62b0263315f9bc662edae1d4cc99dcf17aec54
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-06 18:41:41 +0800

    work

---
+++ page-templates/page-contact-us.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Contact Page
+ */
+
+get_header();
+?>
+<main>
+  <?php
+    while( have_posts() ){
+      the_post();
+
+      $top_fold_banner = get_field('top_fold_banner');
+  ?>
+      <?php get_template_part('page-section/module', 'topFold-page', array('top_fold_banner' => $top_fold_banner)); ?>
+      <?php get_template_part('theme-setting/contact', 'info'); ?>
+  <?php
+    }
+  ?>
+</main>
+<?php get_footer();
