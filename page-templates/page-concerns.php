4c819fd9d42dfa3f2fd52c3aad83b303a3177784
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-06 17:42:52 +0800

    work

--- page-templates/page-concerns.php
+++ page-templates/page-concerns.php
@@ -14,11 +14,53 @@ get_header();
       $top_fold_banner = get_field('top_fold_banner');
   ?>
       <?php get_template_part('page-section/module', 'topFold-page', array('top_fold_banner' => $top_fold_banner)); ?>
-
+      <?php
+        $terms = get_terms('concern-category', array(
+          'hide_empty' => false,
+        ));
+        print_r($terms);
+      ?>
       <div class="grid-list">
         <div class="g">
           <div class="r">
             <div class="lg-12">
+              <?php
+                foreach($terms as $term){
+              ?>
+                  <div id="face" class="grid-list__group" data-grid-cat="face">
+                    <div class="grid-list__tite">
+                      <h2><?php echo $term->name; ?></h2>
+                    </div>
+                    <div class="grid-list__row">
+                      <div class="r rowMargin">
+                        <?php
+                          $the_query = new WP_Query($args);
+                          if($the_query->have_posts()){
+                            while($the_query->have_posts()){
+                              $the_query->the_post();
+                            }
+                          }
+                        ?>
+                        <div class="mdlg-3 md-6">
+                          <div class="grid-list__card">
+                            <div class="grid-list__card-img">
+                              <a href="#" class="eachThumb hoverEffect_dim">
+                                <div class="mediaWrapStyling">
+                                  <img src="assets/demo/small/demo-06.jpg" data-hiResImg="assets/demo/demo-06.jpg" />
+                                </div>
+                              </a>
+                            </div>
+                            <div class="grid-list__card-title">
+                              <p><a href="#">Saggy Skin</a></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              <?php
+                }
+              ?>
               <div id="face" class="grid-list__group" data-grid-cat="face">
                 <div class="grid-list__tite">
                   <h2>Face</h2>
@@ -404,7 +446,6 @@ get_header();
           </div>
         </div>
       </div>
-      
       <?php get_template_part('theme-setting/contact', 'info'); ?>
   <?php
     }
