842879cbe49ea5f62a16ada32924c6cad4f98c82
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-07 11:30:17 +0800

    work

---
+++ single-concern.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<main>
+    <?php
+        while( have_posts() ){
+            the_post();
+
+            $top_fold_banner = get_field('top_fold_banner');
+    ?>
+            <?php get_template_part('page-section/module', 'topFold-concern', array('top_fold_banner' => $top_fold_banner)); ?>
+            <?php get_template_part('theme-setting/contact', 'info'); ?>
+    <?php
+        }
+    ?>
+</main>
+
+<?php get_footer();
