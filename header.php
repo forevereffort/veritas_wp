681441f453a2da76d2087a5b256fffbb8333612a
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-07 23:11:31 +0800

    work

--- header.php
+++ header.php
@@ -4,7 +4,30 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title><?php the_title(); ?> | Veritas</title>
+  <title><?php
+    /*
+    * Print the <title> tag based on what is being viewed.
+    */
+    global $page, $paged;
+
+    wp_title( '|', true, 'right' );
+
+    // Add the blog name.
+    bloginfo( 'name' );
+
+    // Add the blog description for the home/front page.
+    $site_description = get_bloginfo( 'description', 'display' );
+    if ( $site_description && ( is_home() || is_front_page() ) ) {
+      echo " | $site_description";
+    }
+
+      // Add a page number if necessary:
+    if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
+      /* translators: %s: Page number. */
+      echo esc_html( ' | ' . sprintf( __( 'Page %s', 'twentyten' ), max( $paged, $page ) ) );
+    }
+
+  ?></title>
   <meta name="description" content="description">
   <meta name="keywords" content="">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
