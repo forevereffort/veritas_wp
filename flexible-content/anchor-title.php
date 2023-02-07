620a124a35fce821b9485a75d4b01932d999972d
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-07 12:29:05 +0800

    work

--- flexible-content/anchor-title.php
+++ flexible-content/anchor-title.php
@@ -1,7 +1,18 @@
 <?php
     $data = $args['data'];
 ?>
-<div id="<?php echo strtolower($data['anchor_title']); ?>" class="module-section module-anchor-title" data-anchorId="<?php echo strtolower($data['anchor_title']); ?>" data-anchorTitle="<?php echo $data['anchor_title']; ?>">
-  <h5><?php echo $data['anchor_title']; ?></h5>
-  <h3><?php echo $data['section_title']; ?></h3>
+<div id="<?php echo !empty($data['anchor_title']) ? strtolower($data['anchor_title']) : ''; ?>" class="module-section module-anchor-title" data-anchorId="<?php echo !empty($data['anchor_title']) ? strtolower($data['anchor_title']) : ''; ?>" data-anchorTitle="<?php echo !empty($data['anchor_title']) ? $data['anchor_title'] : ''; ?>">
+  <?php
+    if( !empty($data['anchor_title']) ){
+  ?>
+      <h5><?php echo $data['anchor_title']; ?></h5>
+  <?php
+    }
+
+    if( !empty($data['section_title']) ){
+  ?>
+      <h3><?php echo $data['section_title']; ?></h3>
+  <?php
+    }
+  ?>
 </div>
