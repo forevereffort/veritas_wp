2e4b89a2f4d02de6ff89780dc03111e6d4605484
forevereffort <58469967+forevereffort@users.noreply.github.com>
2023-02-08 17:08:58 +0800

    work

--- single-insight.php
+++ single-insight.php
@@ -102,7 +102,7 @@
                               <img src="<?php echo aq_resize($featured_img_url, 50); ?>" data-hiResImg="<?php echo $featured_img_url; ?>" />
                             </div>
                           </a>
-                          <small>Position (optional)</small>
+                          <small><?php echo get_field('author_name'); ?></small>
                           <h4><a href="<?php echo get_the_permalink();?>" class="hoverEffect_dim"><?php the_title(); ?></a></h4>
                           <p><?php the_excerpt(); ?></p>
                         </div>
